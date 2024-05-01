b96fc4b8f72312c64218014f361e4f768b646823
boliny <mohamedelboliny6994@gmail.com>
2024-05-01 09:54:18 +0200

    Changes 10:00 am. Lines starting  On branch master Changes to be committed:

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
         var topDist = $(".container").position();
         if (scroll > topDist.top) {
             $('header').addClass('sticky');
+            $('.main').css({'height' : "calc(100vh + 188px)"});
         } else {
             $('header').removeClass('sticky');
+            $('.main').css({'height' : "calc(100vh)"});
         }
     });
 });
