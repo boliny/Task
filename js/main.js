95781eeea554f4e4f427b1b05693d4fc61592725
boliny <mohamedelboliny6994@gmail.com>
2024-05-02 04:24:16 +0200

    4:30 AM. Lines starting

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
         let topDist = $(".container").position();
         if (scroll > topDist.top) {
             $('header').addClass('sticky');
-            $('.main').css({'height' : "calc(100vh + 188px)"});
-            $('#logo').attr('src', "../images/Layerblack.png");
+            $('.main').css({'height' : "calc(100vh + 180px)"});
+            $('#logo').attr('src', "../images/logoB.png");
 
         } else {
             $('header').removeClass('sticky');
             $('.main').css({'height' : "calc(100vh)"});
-            $('#logo').attr('src', "../images/Layer 0.png");
+            $('#logo').attr('src', "../images/logoW.png");
         }
     });
 });
