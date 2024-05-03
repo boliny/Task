141a7a0a5062d523146daee1406d45c32940efd9
boliny <mohamedelboliny6994@gmail.com>
2024-05-03 08:35:59 +0200

    تعديل الواجهه  Lines starting

--- js/main.js
+++ js/main.js
@@ -4,13 +4,16 @@ $(document).ready(function() {
         let topDist = $(".container").position();
         if (scroll > topDist.top) {
             $('header').addClass('sticky');
-            $('.main').css({'height' : "calc(100vh + 180px)"});
             $('#logo').attr('src', "images/logoB.png");
 
         } else {
             $('header').removeClass('sticky');
-            $('.main').css({'height' : "calc(100vh)"});
             $('#logo').attr('src', "images/logoW.png");
         }
     });
 });
+
+
+$("#flip").click(function () {
+    $("#panel").slideToggle(3000);
+})
